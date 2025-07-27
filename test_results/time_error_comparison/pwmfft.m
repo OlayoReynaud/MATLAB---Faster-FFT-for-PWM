b72334348ad62dd883bdce55b5a15c72b1322975
jpoblaciones <jpoblaci@pa.uc3m.es>
2025-07-27 19:57:41 +0200

    Modifications to minimize the function's error.
    Two modifications have been made:
    
    - The first one, and probably the most relevant one, is in line 11. It just modifies the 0.9999 coefficient, which introduces an error which has to be taken into account when the error's order of magnitude is about 1e-5/1e-6. This coefficient is a patch for a particular error in the algorithm that arises when the modulating signal's value is equal to 1. In this case, the value of kappa (k_act in the paper's notation) takes a non-valid value. This patch can probably be improved with some sort of for loop.
    
    - The second one takes into account the negative spectral components (line 34). It can be proven that C_m(-n) = conj{C_(-m)n}; this implies that, for every C_m(-n) coefficient for which m*m_f - n < 0, there is a C_(-m)n coefficient for which -m*m_f + n > 0; this coefficient, corresponding to the complex conjugate of the negative-frequency C_m(-n) coefficient, will have an impact on the positive side of the spectrum, which is the side of the spectrum we are interested in.
    
    With these changes, the Time-Error characteristic gave reasonable results for both high and low values of m_f.

--- test_results/time_error_comparison/pwmfft.m
+++ test_results/time_error_comparison/pwmfft.m
@@ -8,7 +8,7 @@ function PWM_spectra = pwmfft(v_mod,m_f,num_lobes,num_levels,carrier_phase)
             f = exp(- m * carrier_phase * 1i) * 4/(m*pi) * sin(pi*m/2 * (1 + v_mod));
         else        
             for k = 1 : N    
-                kappa = 1 + floor(0.9999 * (v_mod(k) + 1) * (num_levels - 1)/2);
+                kappa = 1 + floor(0.99999 * (v_mod(k) + 1) * (num_levels - 1)/2);
                 v_mod_kappa = v_mod(k) * (num_levels - 1) + 2 * (ceil((num_levels - 1)/2) - kappa) + mod(num_levels,2);
                 if m == 0
                     f(k) = (4 * (kappa - 1) + 2 * (1 + v_mod_kappa))/(num_levels - 1);                
@@ -31,6 +31,7 @@ function PWM_spectra = pwmfft(v_mod,m_f,num_lobes,num_levels,carrier_phase)
             end
             if floor(N/2) >= m*m_f
                 PWM_spectra(1 : m*m_f) = PWM_spectra(1 : m*m_f) + F(length(F) - m*m_f + 1 : end);
+                PWM_spectra(2 : ceil(N/2) - m*m_f + 2) = PWM_spectra(2 : ceil(N/2) - m*m_f + 2) + conj(F(N - m*m_f : -1 : ceil(N/2)));
             else
                 PWM_spectra(m*m_f - floor(N/2) : m*m_f) = PWM_spectra(m*m_f - floor(N/2) : m*m_f) + F(length(F) - floor(N/2) : end);
             end
@@ -46,5 +47,3 @@ end
 % num_lobes : Number of harmonic lobes to be calculated
 % num_levels : Number of levels of the PWM signal
 % carrier_phase : Array of size (num_levels - 1) containing the phase of each carrier signal
-
-% First attempt to modify this function from MATLAB.
